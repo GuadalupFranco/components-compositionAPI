6445736c7d825b0258a59e62e64dc575fdf27847
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-06-21 18:25:45 -0500

    composition-api

---
+++ src/components/UserRepositories.vue
@@ -0,0 +1,34 @@
+<template>
+    <div>{{ user }} Repositories</div>
+</template>
+
+<script>
+export default {
+    props: {
+        user: {
+            type: String,
+            required: true
+        },
+    },
+    setup(props, ctx) {
+        return {
+            repositories
+        }
+    },
+    data() {
+        return {
+            repositories: [],
+            filters: {},
+            searchQuery: "",
+        };
+    },
+    computed: {
+        filteredRepositories(){
+            return null;
+        },
+        repositoriesMarchingSearchQuery () {
+            return null
+        }
+    }
+}
+</script>
