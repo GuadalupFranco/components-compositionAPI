c450c2e1e4f62ebab9ae320a4d771494a2121299
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-06-22 00:12:59 -0500

    composition api lifecycle

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,14 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png" />
-  <HelloWorld msg="Welcome to Your Vue.js App" />
+  <OneHome></OneHome>
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import OneHome from "./components/OneHome.vue";
 
 export default {
   name: "App",
   components: {
-    HelloWorld,
+    OneHome,
   },
 };
 </script>
