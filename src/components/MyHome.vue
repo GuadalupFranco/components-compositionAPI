35139b9ee4170bdb28a8514dbbd5eec611e37894
GuadalupFranco <franco.ramirez.guadalup@gmail.com>
2022-06-22 16:43:17 -0500

    watchers

--- src/components/MyHome.vue
+++ src/components/MyHome.vue
@@ -1,17 +1,23 @@
 <template>
-  <div>{{ obj }}</div>
+  <div>{{ obj.counter }}</div>
 </template>
 
 <script> 
-import { reactive } from "vue"; // when using normal variables use ref
+import { reactive, watch } from "vue"; // when using normal variables use ref
 
 export default {
   setup() { 
     const obj = reactive({ counter: 0});
     setInterval(() => obj.counter++, 1000);
 
+    watch(
+      () => obj.counter, 
+      (value, previus) => {
+      console.log(value, previus);
+    });
+
     return {
-     obj,
+     obj, 
     };
   },
 };
